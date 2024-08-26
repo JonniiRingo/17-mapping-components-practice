e26f19e877a185c8edfae239bb6157a8f919d5b8
Jonnii Ringo <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-25 21:58:47 -0700

    Successfully completed challenge mapping data to sub components

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import emojipedia from "../emojipedia";
 
 
-function Entry(props){
-    return 
-
-}
+function Entry(props) {
+    return (
+      <div className="term">
+        <dt>
+          <span className="emoji" role="img" aria-label={props.description}> {props.symbol} </span>
+          <span>{props.description}</span>
+        </dt>
+        <dd>
+          {props.definition}
+        </dd>
+      </div>
+    );
+  }
 
 export default Entry;
+
+
